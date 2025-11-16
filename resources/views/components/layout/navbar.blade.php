c1c70f584924d7504c56ad55e22d49254161e542
daudibrahimdev <daudibrahim.dev@gmail.com>
2025-11-16 13:47:54 +0700

    finished modul 4

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,8 +1,6 @@
-<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align
-items-center bg-navbar-theme" 
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" 
     id="layout-navbar"> 
-    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl
-none"> 
+    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none"> 
         <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)"> 
         <i class="bx bx-menu bx-sm"></i> 
         </a> 
@@ -12,11 +10,9 @@ none">
         <ul class="navbar-nav flex-row align-items-center ms-auto"> 
             <!-- User --> 
             <li class="nav-item navbar-dropdown dropdown-user dropdown"> 
-                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data
-bs-toggle="dropdown"> 
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown"> 
                     <div class="avatar avatar-online"> 
-                        <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded
-circle" /> 
+                        <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" /> 
                     </div> 
                 </a> 
                 <ul class="dropdown-menu dropdown-menu-end"> 
@@ -26,8 +22,7 @@ circle" />
                     <div class="d-flex"> 
                         <div class="flex-shrink-0 me-3"> 
                             <div class="avatar avatar-online"> 
-                                <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto 
-rounded-circle" /> 
+                                <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" /> 
                             </div> 
                         </div> 
                         <div class="flex-grow-1"> 
@@ -56,10 +51,16 @@ rounded-circle" />
                     <div class="dropdown-divider"></div> 
                 </li> 
                 <li> 
-                    <a class="dropdown-item" href="auth-login-basic.html"> 
+                    @auth 
+        <form method="POST" action="{{ route('logout') }}" class="d-inline"> 
+          @csrf 
+          <button type="submit" class="btn btn-danger ms-lg-3">Logout</button> 
+        </form> 
+      @endauth 
+                    {{-- <a class="dropdown-item" href="auth-login-basic.html"> 
                         <i class="bx bx-power-off me-2"></i> 
                         <span class="align-middle">Log Out</span> 
-                    </a> 
+                    </a>  --}}
                 </li> 
                 </ul> 
             </li> 
